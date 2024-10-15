56bf1103fef3a535cbe1a055155e0c6eef51999d
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2024-10-15 17:45:05 +0700

    initial commit

---
+++ HIVE/HIVEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIVEApp.swift
+//  HIVE
+//
+//  Created by Austin Xu on 2024/10/15.
+//
+
+import SwiftUI
+
+@main
+struct HIVEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
